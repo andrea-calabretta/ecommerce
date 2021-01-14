d72929b2581dc2d89b8c649fa05cdf7bc9f00380
Andrea Calabretta <andreacalabretta0@gmail.com>
2021-01-14 19:05:15 +0100

    first commit progetto finale

---
+++ calabretta_mauro_payment/payments-manager/src/main/java/calabretta_mauro_proj/paymentsmanager/PaymentsManagerApplication.java
@@ -0,0 +1,13 @@
+package calabretta_mauro_proj.paymentsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentsManagerApplication.class, args);
+    }
+
+}
---
+++ calabretta_mauro_payment/payments-manager/src/test/java/calabretta_mauro_proj/paymentsmanager/PaymentsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package calabretta_mauro_proj.paymentsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
